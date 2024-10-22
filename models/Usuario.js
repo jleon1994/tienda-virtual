c85d2dfcc72074ad8031d70d701cc18450201ee4
jleon1994 <juannicolasleonruiz@gmail.com>
2024-10-21 23:12:49 -0500

    Establecer commit package

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const usuarioSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Usuario = require('../models/Usuario');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 // Ruta de registro
